b15a11b226e136ba4fce1a5db4d1f3a13ec61df9
Jefferson Rylee <rylee.jeff385@gmail.com>
2022-04-16 10:18:56 +0800

    Update implementation Use one line listener and only call saved data listeners Remove input argument for DataListener Remove handling of other events Add methods to accept data and input Accept data or input for start()

--- src/cli.ts
+++ src/cli.ts
@@ -1,27 +1,23 @@
 import { createInterface, Interface } from 'readline';
 import { createImmediate } from './immediate';
 
-export type Listener = (...args: any[]) => void | Promise<void>;
-
-export type DataListener<T = string> = (
-  data: T,
-  input: string
-) => void | Promise<void>;
+export type DataListener<T = string> = (data: T) => void | Promise<void>;
 
 export type ErrorListener = (error: unknown) => void;
 
 export interface CLI<T = string> {
   rl: Interface;
   start(): this;
+  start(isData: true, data: T): this;
+  start(isData: false, input: string): this;
   close(): void;
+  data(data: T): this;
   input(input: string): this;
   ignore(ignore?: boolean): this;
   on(event: 'data', listener: DataListener<T>): this;
   on(event: 'error', listener: ErrorListener): this;
-  on(event: string, listener: Listener): this;
   off(event: 'data', listener: DataListener<T>): this;
   off(event: 'error', listener: ErrorListener): this;
-  off(event: string, listener: Listener): this;
 }
 
 export interface CLIOptions<T> {
@@ -33,18 +29,50 @@ function prop(value: any): PropertyDescriptor {
   return { value, writable: false, enumerable: true, configurable: false };
 }
 
+function errorHandler(error: unknown) {
+  throw error;
+}
+
 export function createCLI<T = string>(options: CLIOptions<T> = {}): CLI<T> {
   const { parser } = options;
   const rl = options.rl || createInterface(process.stdin, process.stdout);
+  let started = false;
   let isIgnoring = false;
   let didSetError = false;
   const cli: CLI<T> = {} as CLI<T>;
-  const listeners = new Map<Listener, Listener[]>();
-  const errorHandler = (error: unknown) => {
-    throw error;
-  };
+  const dataListeners: DataListener<T>[] = [];
   const errorListeners: ErrorListener[] = [errorHandler];
   const immediate = createImmediate();
+
+  const handleInput = async (isData: boolean, input: string | T) => {
+    immediate.clear();
+    if (isIgnoring) {
+      // TODO: clear new history items
+      return;
+    }
+    rl.pause();
+    try {
+      const value =
+        !isData && parser ? await parser(input as string) : (input as T);
+      await Promise.all(dataListeners.map(listener => listener(value)));
+    } catch (error: unknown) {
+      errorListeners.forEach(listener => listener(error));
+    } finally {
+      isIgnoring = false;
+      rl.resume();
+      immediate.set(() => rl.prompt(true));
+    }
+  };
+
+  const data: CLI<T>['data'] = (data: T) => {
+    handleInput(true, data);
+    return cli;
+  };
+
+  const input: CLI<T>['input'] = (input: string) => {
+    handleInput(false, input);
+    return cli;
+  };
 
   const ignore: CLI<T>['ignore'] = (value = true) => {
     isIgnoring = value;
@@ -54,88 +82,52 @@ export function createCLI<T = string>(options: CLIOptions<T> = {}): CLI<T> {
     return cli;
   };
 
-  const start: CLI<T>['start'] = () => {
-    isIgnoring = false;
-    rl.prompt(true);
+  const start: CLI<T>['start'] = (...args: any[]) => {
+    if (started) {
+      return cli;
+    }
+    started = true;
+    // add listener
+    rl.on('line', input => handleInput(false, input));
+    ignore(false);
+    if (args.length > 0) {
+      handleInput(args[0], args[1]);
+    } else {
+      rl.prompt(true);
+    }
     return cli;
   };
 
   const close: CLI<T>['close'] = () => {
     rl.close();
     rl.removeAllListeners();
-    listeners.clear();
+    isIgnoring = false;
     didSetError = false;
+    dataListeners.splice(0);
     errorListeners.splice(0, errorListeners.length, errorHandler);
   };
 
-  const on: CLI<T>['on'] = (event, listener: Listener) => {
-    if (event === 'error') {
-      // remove default error handler
-      if (!didSetError) {
-        didSetError = true;
-        errorListeners.pop();
-      }
-      errorListeners.push(listener);
-      return cli;
+  const on: CLI<T>['on'] = (event, listener) => {
+    // remove default error handler
+    if (event === 'error' && !didSetError) {
+      didSetError = true;
+      errorListeners.pop();
     }
-    const isData = event === 'data';
-    event = isData ? 'line' : event;
-    const isLine = event === 'line';
-    // get existing listener if it exists
-    const handlers = listeners.get(listener);
-    // line events will be paused
-    const wrapped = async (...args: any[]) => {
-      if (isLine) {
-        immediate.clear();
-        if (isIgnoring) {
-          return;
-        }
-        rl.pause();
-      }
-      try {
-        const line = args[0];
-        const all = isData ? [parser ? await parser(line) : line, line] : args;
-        await listener(...all);
-      } catch (error: unknown) {
-        errorListeners.forEach(listener => listener(error));
-      } finally {
-        if (isLine) {
-          isIgnoring = false;
-          rl.resume();
-          immediate.set(() => rl.prompt(true));
-        }
-      }
-    };
-    if (handlers) {
-      handlers.push(wrapped);
-    } else {
-      listeners.set(listener, [wrapped]);
-    }
-    rl.on(event, wrapped);
+    const listeners = event === 'data' ? dataListeners : errorListeners;
+    listeners.push(listener);
     return cli;
   };
 
-  const off: CLI<T>['off'] = (event, listener: Listener): CLI<T> => {
-    if (event === 'error') {
-      const index = errorListeners.indexOf(listener);
-      if (index > -1) {
-        errorListeners.splice(index, 1);
-      }
-      // set default error handler
-      if (errorListeners.length === 0) {
-        didSetError = false;
-        errorListeners.push(errorHandler);
-      }
-      return cli;
+  const off: CLI<T>['off'] = (event, listener): CLI<T> => {
+    const listeners = event === 'data' ? dataListeners : errorListeners;
+    const index = listeners.indexOf(listener);
+    if (index > -1) {
+      listeners.splice(index, 1);
     }
-    event = event === 'data' ? 'line' : event;
-    const handlers = listeners.get(listener);
-    const handler = handlers?.pop() || listener;
-    if (typeof handler === 'function') {
-      rl.off(event, handler);
-    }
-    if (handlers && handlers.length === 0) {
-      listeners.delete(listener);
+    // set default error handler
+    if (event === 'error' && errorListeners.length === 0) {
+      didSetError = false;
+      errorListeners.push(errorHandler);
     }
     return cli;
   };
@@ -144,6 +136,8 @@ export function createCLI<T = string>(options: CLIOptions<T> = {}): CLI<T> {
     rl: prop(rl),
     start: prop(start),
     close: prop(close),
+    data: prop(data),
+    input: prop(input),
     ignore: prop(ignore),
     on: prop(on),
     off: prop(off)
